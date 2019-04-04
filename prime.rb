fa2effb8d1636707ac52efa4c853150ab3110160
Aaron Chu <aaronchu357@gmail.com>
2019-04-04 19:54:25 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime?(number)
+  prime_numbers = []
+  if number < 2
+    return false
+  elsif number == 2
+    prime_numbers << number
+    return true
+  else number % number == 0 && number % prime_numbers != 0
+    prime_numbers << number
+    return true
+  end
+  return false
+end
