31d7f969b5885ce537e613ca0d4507cc281ab892
Aaron Chu <aaronchu357@gmail.com>
2019-04-04 20:15:47 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime?(number)
+  if number < 2
+    return false
+  end
+
+  range = 2...number
+  range.each do |ele|
+    if number % ele == 0
+      return false
+    end
+  end
+  return true
+end
